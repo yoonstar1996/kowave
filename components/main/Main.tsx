e6217abc4aa853edfdb31f3261be50a760cfd15f
Yoonstar <yagobo1110@naver.com>
2025-02-01 14:25:36 +0900

    [Feat] 로딩 수정

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -22,14 +22,10 @@ function Main() {
   const [nowWeather, setNowWeather] = useState<INowWeather | null>(null);
   const [hourlyWeather, setHourlyWeather] = useState<IHourlyWeather[]>([]);
   const [dailyWeather, setDailyWeather] = useState<IDailyWeather[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    let isMounted = true;
-
     const fetchData = async () => {
       try {
-        setIsLoading(true);
         const [nowWeather, hourlyWeatherData, dailyWeather] = await Promise.all(
           [
             fetchTodayWeather(lat, lon),
@@ -38,26 +34,16 @@ function Main() {
           ],
         );
 
-        if (isMounted) {
-          setNowWeather(nowWeather);
-          setHourlyWeather(hourlyWeatherData);
-          setDailyWeather(dailyWeather);
-        }
+        setNowWeather(nowWeather);
+        setHourlyWeather(hourlyWeatherData);
+        setDailyWeather(dailyWeather);
       } catch (error) {
         console.error("날씨 데이터를 가져오는 중 에러 발생:", error);
-      } finally {
-        if (isMounted) setIsLoading(false);
       }
     };
 
     fetchData();
-
-    return () => {
-      isMounted = false;
-    };
   }, [lat, lon]);
-
-  if (isLoading) return <div>날씨 데이터 가져오는 중...</div>;
 
   return (
     <section className="grid grid-cols-1 gap-5 xl:grid-cols-3">
